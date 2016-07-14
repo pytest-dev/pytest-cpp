e8ec1594956de377f8aea7a867ee0244d32f9c37
Leandre Arseneault <leandre.arseneault@cadensimaging.com>
2016-07-14 14:37:26 -0400

    New way to parse xml files

--- pytest_cpp/qt.py
+++ pytest_cpp/qt.py
@@ -43,7 +43,7 @@ class QTestLibFacade(object):
         args = [
             executable,
             "-o",
-            "{xml_file},xunitxml".format(xml_file=log_xml),
+            "{xml_file},xml".format(xml_file=log_xml),
         ]
         p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         stdout, _ = p.communicate()
@@ -62,16 +62,10 @@ class QTestLibFacade(object):
         for file_name in matches:
             tree = ET.parse(file_name)
             test_suite = tree.getroot()
-            failures += int(test_suite.attrib['failures'])
-            tests += int(test_suite.attrib['tests'])
-            errors += int(test_suite.attrib['errors'])
             cases.append(test_suite.getchildren())
             suites.append(test_suite)
 
         new_root = ET.Element('testsuites')
-        new_root.attrib['failures'] = '%s' % failures
-        new_root.attrib['tests'] = '%s' % tests
-        new_root.attrib['errors'] = '%s' % errors
 
         for suite in suites:
             new_root.append(suite)
@@ -112,13 +106,18 @@ class QTestLibFacade(object):
         root = tree.getroot()
         if log:
             for suite in root:
-                if int(root.attrib['errors']) > 0 or int(root.attrib['failures']) > 0:
-                    failed_cases = [case for case in root.iter('testcase') if case.get('result') != "pass"]
+                if len([case for case in root.iter('Incident') if case.get('type') != "pass"]) > 0:
+                    failed_suites = []
+                    failed_cases = [case for case in root.iter('Incident') if case.get('type') != "pass"]
                     updated_attrib = copy.deepcopy(suite.attrib)
                     updated_attrib.update(tests=len(failed_cases))
-                    fail_suite = ET.Element(suite.tag, attrib=updated_attrib)
-                    fail_suite.extend(failed_cases)
-                    failed_suites.append(fail_suite)
+                    #fail_suite = ET.Element(suite.tag, attrib=updated_attrib)
+                    #fail_suite.extend(failed_cases)
+                    #failed_suites.append(fail_suite)
+                    failed_suites = []
+                    failInfo = [(case.get('file'), case.get('line'), list(case)) for case in root.iter('Incident') if case.get('type') != "pass"]
+                    import ipdb; ipdb.set_trace();
+                    failed_suites.append(QTestFailure(failInfo[0][0], int(failInfo[0][1]), failInfo[0][2]))
         return failed_suites
 
 
@@ -126,7 +125,7 @@ class QTestFailure(CppTestFailure):
     def __init__(self, filename, linenum, contents):
         self.filename = filename
         self.linenum = linenum
-        self.lines = contents.splitlines()
+        self.lines = contents
 
     def get_lines(self):
         m = ('red', 'bold')
