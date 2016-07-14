3031e3958747df7ef02ed42d6d90e3a568acde96
Leandre Arseneault <leandre.arseneault@cadensimaging.com>
2016-07-14 10:09:13 -0400

    Log files merged

--- pytest_cpp/qt.py
+++ pytest_cpp/qt.py
@@ -1,10 +1,12 @@
 import os
 import subprocess
 import tempfile
-from xml.etree import ElementTree
 import io
 import shutil
+import copy
+import fnmatch
 from pytest_cpp.error import CppTestFailure
+import xml.etree.ElementTree as ET
 
 
 class QTestLibFacade(object):
@@ -29,7 +31,6 @@ class QTestLibFacade(object):
         return [os.path.basename(os.path.splitext(executable)[0])]
 
     def run_test(self, executable, test_id):
-
         def read_file(name):
             try:
                 with io.open(name) as f:
@@ -47,9 +48,44 @@ class QTestLibFacade(object):
         p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         stdout, _ = p.communicate()
 
+        matches = []
+        for root, dirnames, filenames in os.walk(temp_dir):
+            for filename in filenames:
+                if filename.endswith('.xml'):
+                    matches.append(os.path.join(root, filename))
+        failures = 0
+        tests = 0
+        errors = 0
+        cases = []
+        suites = []
+
+        for file_name in matches:
+            tree = ET.parse(file_name)
+            test_suite = tree.getroot()
+            failures += int(test_suite.attrib['failures'])
+            tests += int(test_suite.attrib['tests'])
+            errors += int(test_suite.attrib['errors'])
+            cases.append(test_suite.getchildren())
+            suites.append(test_suite)
+
+        new_root = ET.Element('testsuites')
+        new_root.attrib['failures'] = '%s' % failures
+        new_root.attrib['tests'] = '%s' % tests
+        new_root.attrib['errors'] = '%s' % errors
+
+        for suite in suites:
+            new_root.append(suite)
+
+        new_tree = ET.ElementTree(new_root)
+        new_tree.write(os.path.join(temp_dir, "result-merged.xml"),
+                       encoding="UTF-8",
+                       xml_declaration=True)
+
+        log_xml = os.path.join(temp_dir, "result-merged.xml")
+
         log = read_file(log_xml)
 
-        if p.returncode not in (0, 200, 201):
+        if p.returncode not in (0, 1):
             msg = ('Internal Error: calling {executable} '
                    'for test {test_id} failed (returncode={returncode}):\n'
                    'output:{stdout}\n'
@@ -64,39 +100,26 @@ class QTestLibFacade(object):
                                     returncode=p.returncode))
             return [failure]
 
-        results = self._parse_log(log=log)
+        results = self._parse_log(log=log_xml)
         shutil.rmtree(temp_dir)
+
         if results:
             return results
 
     def _parse_log(self, log):
-        """
-        Parse the "log" section produced by BoostTest.
-
-        This is always a XML file, and from this we produce most of the
-        failures possible when running BoostTest.
-        """
-        # Fatal errors apparently generate invalid xml in the form:
-        # <FatalError>...</FatalError><TestLog>...</TestLog>
-        # so we have to manually split it into two xmls if that's the case.
-        parsed_elements = []
-        if log.startswith('<FatalError'):
-            fatal, log = log.split('</FatalError>')
-            fatal += '</FatalError>'  # put it back, removed by split()
-            fatal_root = ElementTree.fromstring(fatal)
-            fatal_root.text = 'Fatal Error: %s' % fatal_root.text
-            parsed_elements.append(fatal_root)
-
-        log_root = ElementTree.fromstring(log)
-        parsed_elements.extend(log_root.findall('Exception'))
-        parsed_elements.extend(log_root.findall('Error'))
-
-        result = []
-        for elem in parsed_elements:
-            filename = elem.attrib['file']
-            linenum = int(elem.attrib['line'])
-            result.append(QTestFailure(filename, linenum, elem.text))
-        return result
+        failed_suites = []
+        tree = ET.parse(log)
+        root = tree.getroot()
+        if log:
+            for suite in root:
+                if int(root.attrib['errors']) > 0 or int(root.attrib['failures']) > 0:
+                    failed_cases = [case for case in root.iter('testcase') if case.get('result') != "pass"]
+                    updated_attrib = copy.deepcopy(suite.attrib)
+                    updated_attrib.update(tests=len(failed_cases))
+                    fail_suite = ET.Element(suite.tag, attrib=updated_attrib)
+                    fail_suite.extend(failed_cases)
+                    failed_suites.append(fail_suite)
+        return failed_suites
 
 
 class QTestFailure(CppTestFailure):
