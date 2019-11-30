5848588e8405872ac4c26d530dc49387dfb351cf
Bruno Oliveira <nicoddemus@gmail.com>
2019-11-30 11:41:02 -0300

    Add Python 3.7 and 3.8 to CI and install scons

--- setup.py
+++ setup.py
@@ -31,6 +31,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: C++',
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing',
