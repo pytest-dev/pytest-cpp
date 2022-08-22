788e91be5fba4854b7b84a5a98104916659dd332
Bruno Oliveira <nicoddemus@gmail.com>
2022-08-22 09:31:03 -0300

    Add testing for Python 3.11-dev

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: C++",
         "Topic :: Software Development :: Quality Assurance",
         "Topic :: Software Development :: Testing",
